ce20c976b027600bd20b848d22d29bfcbac1ecb8
Andrew Ren <andrew@Andrews-MacBook-Pro.local>
2022-11-13 14:59:34 -0500

    update tester file

--- templates/init.cpp
+++ templates/init.cpp
@@ -1,6 +1,8 @@
 #include "Icm20948.hpp"
 #include <memory>
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 int main()
 {
@@ -8,8 +10,13 @@ int main()
 
     int i = icm->Initialize();
 
-    std::cout << "Return value: " << i << std::endl;
-
-
+    std::cout << "Initialize Return value: " << i << std::endl;
+    float gyrox;
+    while(true)
+    {
+        gyrox = icm->GetGyroX();
+        std::cout << "Gyro X: " << gyrox << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
     return 0;
 }
